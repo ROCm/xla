a9c553b902a265fc30de337eb8859627bafe5bd9
Zoran Jovanovic <zjovanov@amd.com>
2024-06-14 14:48:10 +0000

    [ROCm] Applied comments from code review.

--- xla/service/gpu/gpu_compiler.cc
+++ xla/service/gpu/gpu_compiler.cc
@@ -1374,7 +1374,7 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
 
     if (debug_options.xla_gpu_enable_triton_gemm() && ((cuda_cc != nullptr &&
         cuda_cc->IsAtLeast(se::CudaComputeCapability::AMPERE)) ||
-        rocm_cc != nullptr)) {
+        rocm_cc)) {
       pipeline.AddPass<GemvRewriter>();
       pipeline.AddPass<GemmFusion>(gpu_version);
     }
