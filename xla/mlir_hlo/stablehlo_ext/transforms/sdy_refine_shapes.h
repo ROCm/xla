tensorflow/mlir-hlo

#sdy support StableHLO from refining Shardy ops with polymorphic shapes
PiperOrigin-RevId: 716261489

stablehlo_ext/transforms/sdy_refine_shapes.h
@@ -1,0 +1,32 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_SDY_REFINE_SHAPES_H_
+#define TENSORFLOW_COMPILER_XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_SDY_REFINE_SHAPES_H_
+
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/PatternMatch.h"
+
+namespace mlir {
+namespace stablehlo_ext {
+
+/// Populates extension patterns for refining shapes of Shardy ops.
+void populateSdyShapeRefinementPatterns(RewritePatternSet* patterns,
+                                        MLIRContext* context);
+
+}  // namespace stablehlo_ext
+}  // namespace mlir
+
+#endif  // TENSORFLOW_COMPILER_XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_SDY_REFINE_SHAPES_H_

stablehlo_ext/transforms/stablehlo_refine_shapes.h
@@ -1,0 +1,32 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_STABLEHLO_REFINE_SHAPES_H_
+#define TENSORFLOW_COMPILER_XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_STABLEHLO_REFINE_SHAPES_H_
+
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/PatternMatch.h"
+
+namespace mlir {
+namespace stablehlo_ext {
+
+// Populates extension patterns for refining shapes of StableHLO and Shardy ops.
+void populateStablehloExtRefineShapesPatterns(RewritePatternSet *patterns,
+                                              MLIRContext *context);
+
+}  // namespace stablehlo_ext
+}  // namespace mlir
+
+#endif  // TENSORFLOW_COMPILER_XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_STABLEHLO_REFINE_SHAPES_H_

ROCm/xla

xla/mlir_hlo/stablehlo_ext/transforms/sdy_refine_shapes.h
@@ -1,0 +1,32 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_SDY_REFINE_SHAPES_H_
+#define XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_SDY_REFINE_SHAPES_H_
+
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/PatternMatch.h"
+
+namespace mlir {
+namespace stablehlo_ext {
+
+/// Populates extension patterns for refining shapes of Shardy ops.
+void populateSdyShapeRefinementPatterns(RewritePatternSet* patterns,
+                                        MLIRContext* context);
+
+}  // namespace stablehlo_ext
+}  // namespace mlir
+
+#endif  // XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_SDY_REFINE_SHAPES_H_

xla/mlir_hlo/stablehlo_ext/transforms/stablehlo_refine_shapes.h
@@ -1,0 +1,32 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_STABLEHLO_REFINE_SHAPES_H_
+#define XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_STABLEHLO_REFINE_SHAPES_H_
+
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/PatternMatch.h"
+
+namespace mlir {
+namespace stablehlo_ext {
+
+// Populates extension patterns for refining shapes of StableHLO and Shardy ops.
+void populateStablehloExtRefineShapesPatterns(RewritePatternSet *patterns,
+                                              MLIRContext *context);
+
+}  // namespace stablehlo_ext
+}  // namespace mlir
+
+#endif  // XLA_MLIR_HLO_STABLEHLO_EXT_TRANSFORMS_STABLEHLO_REFINE_SHAPES_H_