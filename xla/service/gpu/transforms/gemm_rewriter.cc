8b94b20ea459d74fa3af5651c800c8fef620309a
scxfjiang <xuefei.jiang@amd.com>
2025-05-18 14:19:48 -0400

    adjust workspace size for gfx942

--- xla/service/gpu/transforms/gemm_rewriter.cc
+++ xla/service/gpu/transforms/gemm_rewriter.cc
@@ -2529,8 +2529,9 @@ class GemmWorkspaceRewriteVisitor : public DfsHloRewriteVisitor {
       workspace = GemmConfig::kHopperWorkspace;
     }
     auto *rocm_cc = std::get_if<se::RocmComputeCapability>(&gpu_version_);
-    if (rocm_cc != nullptr && rocm_cc->gfx_version() == "gfx950") {
-      workspace = GemmConfig::kGFX950Workspace;
+    if (rocm_cc != nullptr &&
+        (rocm_cc->gfx_version() == "gfx950" || rocm_cc->gfx9_mi300())) {
+      workspace = GemmConfig::kROCmStreamKWorkspace;
     }
 
     // We do not know the workspace size required by cuBLAS, but we can guess
