e26ec8e7304c8b93759deb665e53052997ac4588
Gabe Weisz <gabe.weisz@amd.com>
2025-05-20 19:02:40 +0000

    fix compile error

--- xla/service/gpu/matmul_utils.h
+++ xla/service/gpu/matmul_utils.h
@@ -82,6 +82,10 @@ struct GemmConfig : public se::gpu::GemmConfig {
   // Size of the workspace based on NVIDIA recommendation:
   // https://docs.nvidia.com/cuda/cublas/#cublassetworkspace
   static constexpr int64_t kHopperWorkspace = 32 * 1024 * 1024;  // 32 MiB
+<<<<<<< HEAD
+=======
+  static constexpr int64_t kGFX950Workspace = 72 * 1024 * 1024;  // 72 MiB
+>>>>>>> 3c1a80571f (fix compile error)
   static constexpr int64_t kDefaultWorkspace = 4 * 1024 * 1024;  // 4 MiB
 
   static absl::StatusOr<GemmConfig> For(
