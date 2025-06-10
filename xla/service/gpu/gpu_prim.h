a008dd49b7aa97bb82e3c23ce43ef5a419259d6a
Chao Chen <cchen104@amd.com>
2025-06-10 08:42:39 -0500

    rocprim patch for rocm70

--- xla/service/gpu/gpu_prim.h
+++ xla/service/gpu/gpu_prim.h
@@ -40,6 +40,7 @@ namespace gpuprim = ::hipcub;
 
 // Required for sorting Eigen::half and bfloat16.
 namespace rocprim {
+#if (TF_ROCM_VERSION >= 50200 && TF_ROCM_VERSION < 70000)
 namespace detail {
 
 #if (TF_ROCM_VERSION >= 50200)
@@ -58,7 +59,32 @@ struct float_bit_mask<tsl::bfloat16> {
   static constexpr uint16_t mantissa = 0x007F;
   using bit_type = uint16_t;
 };
-#endif  // TF_ROCM_VERSION >= 50200
+}; // namespace detail
+#else // TF_ROCM_VERSION >= 70000
+namespace traits {
+
+template<>
+struct rocprim::traits::define<Eigen::half> {
+  using float_bit_mask = 
+              rocprim::traits::float_bit_mask::values<uint16_t, 0x8000, 0x7C00, 0x03FF>;
+  using is_arithmetic = rocprim::traits::is_arithmetic::values<true>;
+  using number_format = 
+            rocprim::traits::number_format::values<traits::number_format::kind::floating_point_type>;
+};
+
+template<>
+struct rocprim::traits::define<tsl::bfloat16> {
+  using float_bit_mask = rocprim::traits::float_bit_mask::values<uint16_t, 0x8000, 0x7F80, 0x007F>;
+  using is_arithmetic = rocprim::traits::is_arithmetic::values<true>;
+  using number_format = 
+          rocprim::traits::number_format::values<traits::number_format::kind::floating_point_type>;
+};
+
+}; // namespace traits
+#endif  // TF_ROCM_VERSION >= 50200 && TF_ROCM_VERSION < 70000
+
+#if (TF_ROCM_VERSION < 70000)
+namespace detail {
 template <>
 struct radix_key_codec_base<Eigen::half>
     : radix_key_codec_floating<Eigen::half, uint16_t> {};
@@ -66,6 +92,8 @@ template <>
 struct radix_key_codec_base<tsl::bfloat16>
     : radix_key_codec_floating<tsl::bfloat16, uint16_t> {};
 };  // namespace detail
+#endif // TF_ROCM_VERSION < 70000
+
 };  // namespace rocprim
 
 #endif  // TENSORFLOW_USE_ROCM
