a815a25ef9b6ff55fc61ade2a321d46fb50c8a53
Alexandros Theodoridis <atheodor@amd.com>
2025-10-07 13:43:09 +0000

    Trigger CI/CD pipeline

--- build_tools/rocm/run_xla.sh
+++ build_tools/rocm/run_xla.sh
@@ -136,7 +136,7 @@ bazel --bazelrc=build_tools/rocm/rocm_xla.bazelrc test \
     --local_test_jobs=${N_TEST_JOBS} \
     --test_env=TF_TESTS_PER_GPU=$TF_TESTS_PER_GPU \
     --test_env=TF_GPU_COUNT=$TF_GPU_COUNT \
-    --action_env=TF_ROCM_AMDGPU_TARGETS=gfx908,gfx942 \
+    --action_env=TF_ROCM_AMDGPU_TARGETS=gfx908,gfx942,gfx90a \
     --action_env=XLA_FLAGS="--xla_gpu_enable_llvm_module_compilation_parallelism=true --xla_gpu_force_compilation_parallelism=16" \
     --run_under=//build_tools/ci:parallel_gpu_execute \
     --test_env=MIOPEN_FIND_ENFORCE=5 \
