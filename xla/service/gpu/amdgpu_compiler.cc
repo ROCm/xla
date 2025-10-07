91140dd50268f9c05f191bf180ef924843a2d0be
Harsha H S <hsharsha@users.noreply.github.com>
2025-10-07 09:12:19 +0100

    [ROCm] Restore CudnnFusedConvRewriter  (#372) (#392)
    * [ROCm] Allways run GpuConvAlgorithmPicker
    
    * [ROCm] Restore CudnnFusedConvRewriter
    
    Introduce CudnnFusedConvDecomposer to revert back fused convs
    if no fused algorithm could be found with ConvAlgorithmPicker.
    Remove unfused fallback paths from RocmFusedConvRunner.
    
    Co-authored-by: Dragan Mladjenovic <dragan.mladjenovic@amd.com>

--- xla/service/gpu/amdgpu_compiler.cc
+++ xla/service/gpu/amdgpu_compiler.cc
@@ -61,6 +61,7 @@ limitations under the License.
 #include "xla/service/gpu/transforms/conv_rewriter.h"
 #include "xla/service/gpu/transforms/cublas_pad_for_gemms.h"
 #include "xla/service/gpu/transforms/cudnn_fused_conv_rewriter.h"
+#include "xla/service/gpu/transforms/cudnn_fused_conv_decomposer.h"
 #include "xla/service/gpu/transforms/gpusolver_rewriter.h"
 #include "xla/service/gpu/transforms/triangular_solve_rewriter.h"
 #include "xla/service/hlo_module_config.h"
@@ -238,7 +239,7 @@ absl::Status AMDGPUCompiler::OptimizeHloPostLayoutAssignment(
 // enabled.
 bool AMDGPUCompiler::RequiresCollectiveScheduleLinearizer(
     const HloModule* module, se::StreamExecutor* stream_exec) {
-  if (stream_exec == nullptr || !GpuConvAlgorithmPicker::IsEnabled(module)) {
+  if (stream_exec == nullptr) {
     return false;
   }
   for (const HloComputation* comp : module->MakeNonfusionComputations()) {
@@ -256,40 +257,16 @@ absl::Status AMDGPUCompiler::AddConvAndGemmAutotuningPasses(
     HloPassPipeline* pipeline, const se::GpuComputeCapability& gpu_version,
     const CompileOptions& options, HloModule* hlo_module,
     AutotuneConfig& autotune_config, tsl::thread::ThreadPool* thread_pool,
-    se::StreamExecutor* stream_exec,
-    const Compiler::TargetConfig* target_config) {
-  const DebugOptions& debug_options = hlo_module->config().debug_options();
-  if (hlo_module->config()
-          .debug_options()
-          .xla_gpu_experimental_disable_binary_libraries() ||
-      debug_options.xla_gpu_autotune_level() == 0 ||
-      debug_options.xla_gpu_exclude_nondeterministic_ops() ||
-      stream_exec == nullptr) {
-    return absl::OkStatus();
-  }
-
   // TODO(b/407494793): Remove the GpuConvAlgorithmPicker and use the autotuner
   // it supports ROCM.
   pipeline->AddPass<GpuConvAlgorithmPicker>(autotune_config);
 
-  std::vector<std::unique_ptr<CodegenBackend>> backends;
-  // TODO(b/407494793): - Add proper support for ROCM. Currently the Cublas
-  // backend uses the same API as rocBLAS.
-  backends.push_back(std::make_unique<CublasBackend>(
-      stream_exec, &debug_options, this, target_config));
-  backends.push_back(std::make_unique<CublasLtBackend>(
-      stream_exec, &debug_options, this, target_config));
-  auto should_autotune = [](const HloInstruction& instruction) -> bool {
-    return instruction.opcode() == HloOpcode::kCustomCall &&
-           IsCublasGemm(instruction);
-  };
-  TF_ASSIGN_OR_RETURN(
-      std::unique_ptr<AutotunerPass> autotuner_pass,
-      AutotunerPass::Create(std::move(backends), debug_options, stream_exec,
-                            thread_pool, should_autotune, target_config,
-                            options.device_allocator));
-  pipeline->AddPass(std::move(autotuner_pass));
-
+    se::StreamExecutor* stream_exec) {
+  //We do autotuning irrespective of --xla_gpu_autotune_level.
+  pipeline->AddPass<GpuConvAlgorithmPicker>(autotune_config);
+  // Undo CudnnFusedConvRewriter work if no algorithm was found.
+  pipeline->AddPass<CudnnFusedConvDecomposer>();
+  pipeline->AddPass<GemmAlgorithmPicker>(autotune_config);
   return absl::OkStatus();
 }
 
