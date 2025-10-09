4f9d77f160e4576c5de433ede02282553579e202
Alexandros Theodoridis <atheodor@amd.com>
2025-10-09 16:02:29 +0000

    Address review comments

--- build_tools/rocm/run_xla_multi_gpu.sh
+++ build_tools/rocm/run_xla_multi_gpu.sh
@@ -99,7 +99,6 @@ bazel --bazelrc=build_tools/rocm/rocm_xla.bazelrc test \
     --flaky_test_attempts=3 \
     --keep_going \
     --test_strategy=exclusive \
-    --action_env=TF_ROCM_AMDGPU_TARGETS=gfx908,gfx942,gfx90a \
     --action_env=XLA_FLAGS=--xla_gpu_force_compilation_parallelism=16 \
     --action_env=XLA_FLAGS=--xla_gpu_enable_llvm_module_compilation_parallelism=true \
     --action_env=NCCL_MAX_NCHANNELS=1 \
