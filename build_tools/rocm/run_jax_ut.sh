c4e4dbd74c56c62be0088ada08f9a854eb7e84fd
Alexandros Theodoridis <atheodor@amd.com>
2025-10-20 09:02:07 +0000

    Add rbe settings manually

--- build_tools/rocm/run_jax_ut.sh
+++ build_tools/rocm/run_jax_ut.sh
@@ -5,23 +5,20 @@ set -e
 JAX_DIR=$1
 XLA_DIR=$(pwd)
 
-SCRIPT_DIR=$(realpath $(dirname $0))
 pushd $JAX_DIR
 
 python build/build.py build \
-        --wheels=jax-rocm-plugin \
-        --configure_only \
-        --local_xla_path=${XLA_DIR} \
-        --python_version=3.12
+    --wheels=jax-rocm-plugin \
+    --configure_only \
+    --local_xla_path=${XLA_DIR} \
+    --python_version=3.12
 
 bazel test \
     --config=rocm \
-    --config=rocm_rbe \
     --build_tag_filters=cpu,gpu,-tpu,-config-cuda-only \
     --test_tag_filters=cpu,gpu,-tpu,-config-cuda-only \
-    --action_env=TF_ROCM_AMDGPU_TARGETS=gfx908 \
+    --action_env=TF_ROCM_AMDGPU_TARGETS=gfx908,gfx90a,gfx942 \
     --//jax:build_jaxlib=true \
-    --test_output=streamed \
     "//tests/..."
 
 popd
