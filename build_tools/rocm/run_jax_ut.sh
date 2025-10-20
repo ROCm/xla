fe9ecc7f13a2780eb4e2d453e559d3bfe24fd735
Alexandros Theodoridis <atheodor@amd.com>
2025-10-20 07:14:42 +0000

    Adjust tag filters

--- build_tools/rocm/run_jax_ut.sh
+++ build_tools/rocm/run_jax_ut.sh
@@ -5,6 +5,7 @@ set -e
 JAX_DIR=$1
 XLA_DIR=$(pwd)
 
+SCRIPT_DIR=$(realpath $(dirname $0))
 pushd $JAX_DIR
 
 python build/build.py build \
@@ -15,6 +16,8 @@ python build/build.py build \
 
 bazel test \
     --config=rocm \
+    --build_tag_filters=cpu,gpu,-tpu,-config-cuda-only \
+    --test_tag_filters=cpu,gpu,-tpu,-config-cuda-only \
     --action_env=TF_ROCM_AMDGPU_TARGETS=gfx908 \
     --//jax:build_jaxlib=true \
     --test_output=streamed \
