1b2637d2683757e5daf2b5fbda770e8cf055f6cb
Alexandros Theodoridis <atheodor@amd.com>
2025-12-02 20:53:21 +0000

    Include all multigpu tests

--- build_tools/rocm/run_xla_ci_build.sh
+++ build_tools/rocm/run_xla_ci_build.sh
@@ -95,7 +95,6 @@ bazel --bazelrc="$SCRIPT_DIR/rocm_xla.bazelrc" test \
     -//xla/tests:scatter_test_amdgpu_any \
     -//xla/tests:scatter_test_amdgpu_any_notfrt \
     -//xla/tools/hlo_opt:tests/gpu_hlo_llvm.hlo.test \
-    -//xla/tests:collective_pipeline_parallelism_test_amdgpu_any \
     -//xla/backends/gpu/codegen/triton:dot_algorithms_legacy_test_amdgpu_any \
     -//xla/tests:cholesky_test_amdgpu_any \
     -//xla/service/gpu/tests:sorting.hlo.test \
@@ -106,16 +105,6 @@ bazel --bazelrc="$SCRIPT_DIR/rocm_xla.bazelrc" test \
     -//xla/hlo/builder/lib:self_adjoint_eig_test_amdgpu_any_notfrt \
     -//xla/tests:convert_test_amdgpu_any \
     -//xla/pjrt/gpu/tfrt:tfrt_gpu_buffer_test \
-    -//xla/backends/gpu/collectives:nvshmem_collectives_test_amdgpu_any \
-    -//xla/pjrt/gpu:nvshmem_gpu_collectives_test_amdgpu_any \
     -//xla/service/gpu:auto_sharding_gpu_compiler_test_amdgpu_any \
-    -//xla/backends/gpu/codegen/triton:fusion_emitter_large_test_amdgpu_any \
-    -//xla/pjrt/gpu:se_gpu_pjrt_client_test_amdgpu_any \
-    -//xla/backends/gpu/codegen:dynamic_slice_fusion_test_amdgpu_any \
-    -//xla/pjrt/gpu:pjrt_client_test_se_gpu_amdgpu_any \
-    -//xla/tests:collective_ops_e2e_test_amdgpu_any \
-    -//xla/tests:collective_ops_test_amdgpu_any \
-    -//xla/tools/multihost_hlo_runner:functional_hlo_runner_test_amdgpu_any \
-    -//xla/pjrt/gpu:se_gpu_pjrt_client_nvshmem_test_amdgpu_any \
-    -//xla/tools/multihost_hlo_runner:functional_hlo_runner_test_amdgpu_any
+    -//xla/backends/gpu/codegen/triton:fusion_emitter_large_test_amdgpu_any
 
