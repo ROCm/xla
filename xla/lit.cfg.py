f8d828abd250a94eed6e04b0398edb96b54ac947
Aleksei Nurmukhametov <alex@nurmukhametov.ru>
2026-01-23 15:21:17 +0000

    Remove extra import in xla/lit.cfg.py (#560)
    There is same import a few lines below that was added in the upstream in
    https://github.com/openxla/xla/pull/34150

--- xla/lit.cfg.py
+++ xla/lit.cfg.py
@@ -19,8 +19,6 @@ import tempfile
 
 import lit.formats
 
-from xla.sh_test_with_runfiles import ShTestWithRunfiles
-
 # copybara:uncomment_begin(google-only)
 # from xla.lit_google_cfg import ENV_FLAGS as google_env_flags
 # copybara:uncomment_end
