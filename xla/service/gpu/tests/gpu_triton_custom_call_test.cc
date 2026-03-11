f0f11f28acdeee88ff7564f42e3c4286a14bf206
zoranjovanovic-ns <126815388+zoranjovanovic-ns@users.noreply.github.com>
2026-03-11 05:14:40 -0700

    PR #38801: [ROCm] Skip CanNotEmitTritonCustomCallOnPreAmpereGpu in gpu_triton_cu…
    Imported from GitHub PR https://github.com/openxla/xla/pull/38801
    
    …stom_call_test for ROCm
    
    📝 Summary of Changes
    skipped CanNotEmitTritonCustomCallOnPreAmpereGpu in gpu_triton_custom_call_test for ROCm
    
    🎯 Justification
    Unit test was failing because it works on ROCm
    
    🚀 Kind of Contribution
    Please remove what does not apply:  🧪 Tests
    
    📊 Benchmark (for Performance Improvements)
    Please measure and include speedups for one of the public HLOs in
    `compiler/xla/tools/benchmarks/hlo/`.
    
    🧪 Unit Tests:
    gpu_triton_custom_call_test
    
    🧪 Execution Tests:
    What execution tests were added? For example, a new optimization should be
    tested with an end-to-end execution test triggering the optimization and
    asserting correctness. Please provide test cases running with at most 2 GPUs.
    
    Copybara import of the project:
    
    --
    6cf15ace1e8afee938aa9e1c6e7c99762ef4cfa6 by Zoran Jovanovic <zjovanov@amd.com>:
    
    [ROCm] Skip CanNotEmitTritonCustomCallOnPreAmpereGpu in gpu_triton_custom_call_test for ROCm
    
    Merging this change closes #38801
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/38801 from ROCm:rocm-fix-gpu_triton_custom_call_test 6cf15ace1e8afee938aa9e1c6e7c99762ef4cfa6
    PiperOrigin-RevId: 881953512

--- xla/service/gpu/tests/gpu_triton_custom_call_test.cc
+++ xla/service/gpu/tests/gpu_triton_custom_call_test.cc
@@ -137,6 +137,13 @@ class GpuIrEmitterUnnestedTest : public GpuPjRtCodegenTest {
   se::GpuComputeCapability GetGpuComputeCapability() {
     return device_description().gpu_compute_capability();
   }
+
+  se::GpuComputeCapability GetGpuComputeCapability() {
+    return backend()
+           .default_stream_executor()
+           ->GetDeviceDescription()
+           .gpu_compute_capability();
+  }
 };
 
 TEST_F(GpuIrEmitterUnnestedTest, EmitTritonCustomCallWithCorrectLowering) {
