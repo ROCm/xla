bece5eefd4ea0cbe07359013429339a1bfd25fc0
Chao <cchen104@amd.com>
2026-04-21 12:43:04 +0100

    Update skipped list for upstream CI (#791)
    * Update execute_ci_build_upstream.sh
    
    * Enable //xla/service/gpu:float_support_test_amdgpu_any
    
    * Enable //xla/backends/gpu/transforms:triton_fusion_numerics_verifier_test_amdgpu_any
    
    * Update mgpu tests and exclusion list for sgpu tests
    
    * Exclude Numa test as it fails on RBE and use EXCLUDE_TARGETS variable
    
    * Fix sgpu and mgpu exclusions correctly
    
    * Use tags to filter mgpu, sgpu tests
    
    * Update exclusion list for sgpu tagets
    
    * Update execute_ci_build_upstream.sh
    
    * Update TAG_FILTERS for multi_gpu and gpu configurations
    
    no-oss is already inside
    
    ---------
    
    Co-authored-by: Jian Li <Jian.Li@amd.com>
    Co-authored-by: Harsha HS <Harsha.HavanurShamsundara@amd.com>
    Co-authored-by: Harsha H S <hsharsha@users.noreply.github.com>
    Co-authored-by: Alexandros Theodoridis <alexandros.theodoridis@amd.com>

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -5,70 +5,25 @@ set -ex
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 
 EXCLUDED_TESTS=(
-    "*ParametersUsedByCollectiveMosaicShouldBeCopiedToCollectiveMemory"
-    "SortingTest*"
-    "*IotaR1Test*"
-    "HostMemoryAllocateTest.Numa"
-    "CubSort*"
-    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e4m3fn_f8e5m2"
-    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e5m2_f8e5m2"
-    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e5m2_f8e4m3fn"
-    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e4m3fn_f8e4m3fn"
-)
-
-EXCLUDED_TARGETS_SGPU=(
-    "//xla/service/gpu:dot_algorithm_support_test_amdgpu_any"
-    "//xla/service/gpu:float_support_test_amdgpu_any"
-    "//xla/backends/gpu/transforms:scatter_determinism_expander_test_amdgpu_any"
-    "//xla/backends/gpu/transforms:triton_fusion_numerics_verifier_test_amdgpu_any"
-    "//xla/backends/gpu/codegen/triton:dot_algorithms_test_amdgpu_any"
-)
-
-TEST_TARGETS_SGPU=(
-    "//xla/..."
-    "-//xla/service/gpu:dot_algorithm_support_test_amdgpu_any"
-    "-//xla/service/gpu:float_support_test_amdgpu_any"
-    "-//xla/backends/gpu/transforms:scatter_determinism_expander_test_amdgpu_any"
-    "-//xla/backends/gpu/transforms:triton_fusion_numerics_verifier_test_amdgpu_any"
-    "-//xla/backends/gpu/codegen/triton:dot_algorithms_test_amdgpu_any"
-)
-
-TEST_TARGETS_MGPU=(
-    "//xla/backends/gpu/tests:collective_pipeline_parallelism_test"
-    "//xla/backends/gpu/collectives:gpu_clique_key_test"
-    "//xla/service:collective_ops_utils_test"
-    "//xla/service:collective_pipeliner_test"
-    "//xla/service:collective_permute_cycle_test"
-    "//xla/service:batched_gather_scatter_normalizer_test"
-    "//xla/service:all_reduce_simplifier_test"
-    "//xla/service:all_gather_simplifier_test"
-    "//xla/service:reduce_scatter_decomposer_test"
-    "//xla/service:reduce_scatter_reassociate_test"
-    "//xla/service:reduce_scatter_combiner_test"
-    "//xla/service:scatter_simplifier_test"
-    "//xla/service:sharding_propagation_test"
-    "//xla/service:sharding_remover_test"
-    "//xla/service:p2p_schedule_preparation_test"
-    "//xla/pjrt/distributed:topology_util_test"
-    "//xla/pjrt/distributed:client_server_test"
+    "HostMemoryAllocateTest.Numa" # Failing on RBE
+    "*IotaR1Test*" # Taking too many CI nodes
+    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e4m3fn_f8e5m2" # TODO: fix
+    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e5m2_f8e5m2" # TODO: fix
+    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e5m2_f8e4m3fn" # TODO: fix
+    "Fp8s/FloatNormalizationTest.Fp8Normalization/f8e4m3fn_f8e4m3fn" # TODO: fix
+    "TritonAndBlasSupportForDifferentTensorSizes/TritonAndBlasSupportForDifferentTensorSizes.IsDotAlgorithmSupportedByTriton/dot_bf16_bf16_f32_x6" # TODO: fix
+    "TritonAndBlasSupportForDifferentTensorSizes/TritonAndBlasSupportForDifferentTensorSizes.IsDotAlgorithmSupportedByTriton/dot_bf16_bf16_f32_x9" # TODO: fix
+    "RocmExecutorTest.CreateUnifiedMemoryAllocatorWorks" # TODO: fix
 )
 
 TAG_FILTERS=$("${SCRIPT_DIR}/rocm_tag_filters.sh")
-TEST_TARGETS=("${TEST_TARGETS_SGPU[@]}")
-SGPU_AMDGPU_TARGETS="${TF_ROCM_SGPU_AMDGPU_TARGETS:-gfx90a,gfx942}"
-MGPU_AMDGPU_TARGETS="${TF_ROCM_MGPU_AMDGPU_TARGETS:-gfx950}"
-AMDGPU_TARGETS="${SGPU_AMDGPU_TARGETS}"
 
 for arg in "$@"; do
     if [[ "$arg" == "--config=ci_multi_gpu" ]]; then
-        TAG_FILTERS=""
-        TEST_TARGETS=("${TEST_TARGETS_MGPU[@]}")
-        AMDGPU_TARGETS="${MGPU_AMDGPU_TARGETS}"
+        TAG_FILTERS="${TAG_FILTERS},multi_gpu"
     fi
     if [[ "$arg" == "--config=ci_single_gpu" ]]; then
-        TAG_FILTERS="${TAG_FILTERS},gpu,-multi_gpu,-no_oss"
-        TEST_TARGETS=("${TEST_TARGETS_SGPU[@]}")
-        AMDGPU_TARGETS="${SGPU_AMDGPU_TARGETS}"
+        TAG_FILTERS="${TAG_FILTERS},gpu,-multi_gpu"
     fi
 done
 
@@ -79,7 +34,7 @@ done
     --execution_log_compact_file=execution_log.binpb.zst \
     --spawn_strategy=local \
     --repo_env=REMOTE_GPU_TESTING=1 \
-    --repo_env=TF_ROCM_AMDGPU_TARGETS=${AMDGPU_TARGETS} \
+    --repo_env=TF_ROCM_AMDGPU_TARGETS=gfx90a,gfx942,gfx950 \
     --remote_download_outputs=minimal \
     --grpc_keepalive_time=30s \
     --test_sharding_strategy=disabled \
@@ -90,4 +45,7 @@ done
         IFS=:
         echo "${EXCLUDED_TESTS[*]}"
     ) \
-    -- "${TEST_TARGETS[@]}"
+    -- \
+    //xla/... \
+    -//xla/backends/gpu/tests:sorting.hlo.test_mi200 # lit test can't filter with gtest filters
+
