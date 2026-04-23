16ba1a3c1715740ddd4c541ddde8193319b71d83
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-04-23 09:40:19 +0200

    Add dynamic config for ROCM CI build execution

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -51,6 +51,7 @@ done
 
 "${SCRIPT_DIR}/run_xla_ci_build.sh" \
     "$@" \
+    --config=rocm_rbe_dynamic \
     --build_tag_filters="$TAG_FILTERS" \
     --test_tag_filters="$TAG_FILTERS" \
     --execution_log_compact_file=execution_log.binpb.zst \
