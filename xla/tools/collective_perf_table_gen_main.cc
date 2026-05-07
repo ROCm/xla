3f05fb4bf6e792e3df7eed235d823294b33eb145
Maxime France-Pillois <mfrancep@amd.com>
2026-05-07 11:23:59 +0100

    Add missing headers and dependencies

--- xla/tools/collective_perf_table_gen_main.cc
+++ xla/tools/collective_perf_table_gen_main.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/strings/ascii.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
