ROCm/xla

commit cd5431d198932d9cc8d900b321532a36f28842fa

[ROCm] Streamline bazel targets for rocm libraries (#805)
* [ROCm] Streamline bazel targets for rocm libraries

* Move to rocm_lib_import macro

* [ROCm] Unblock CI after PR #40385

xla/backends/profiler/gpu/BUILD
@@ -451,6 +451,11 @@ cc_library(
         ":use_rocprofiler_sdk": ["XLA_GPU_ROCM_TRACER_BACKEND=3"],
         "//conditions:default": ["XLA_GPU_ROCM_TRACER_BACKEND=3"],
     }),
+    deps = select({
+        ":use_v1": ["@local_config_rocm//rocm:roctracer"],
+        ":use_rocprofiler_sdk": ["@local_config_rocm//rocm:rocprofiler_sdk"],
+        "//conditions:default": ["@local_config_rocm//rocm:rocprofiler_sdk"],
+    })
     visibility = ["//visibility:public"],
 )
 
@@ -477,7 +482,6 @@ cc_library(
     "@tsl//tsl/platform:errors",
     "@tsl//tsl/platform:logging",
     "@tsl//tsl/platform:macros",
-    "@local_config_rocm//rocm:rocprofiler-sdk",
   ],
   visibility = ["//visibility:public"],
 )
@@ -520,7 +524,6 @@ cc_library(
         "@tsl//tsl/platform:types",
         "@tsl//tsl/profiler/lib:profiler_factory",
         "@tsl//tsl/profiler/lib:profiler_interface",
-        "@local_config_rocm//rocm:rocprofiler-sdk",
     ],
 )
 

xla/backends/profiler/gpu/rocm_collector.cc
@@ -35,6 +35,7 @@ limitations under the License.
 #include "rocm/include/hip/hip_runtime.h"
 #include "rocm/include/rocprofiler-sdk/fwd.h"
 #include "rocm/include/rocprofiler-sdk/rocprofiler.h"
+#include "rocm/include/roctracer/roctracer.h"
 #include "xla/backends/profiler/gpu/rocm_tracer_utils.h"
 #include "xla/tsl/platform/status.h"
 #include "xla/tsl/profiler/utils/parse_annotation.h"

xla/service/gpu/BUILD
@@ -2585,7 +2585,6 @@ cc_library(
         "//xla/stream_executor:stream_executor_h",
         "//xla/stream_executor/rocm:rocm_compute_capability",
         "//xla/stream_executor/rocm:rocm_platform_id",
-        "//xla/stream_executor/rocm:rocm_solver_context",
         "//xla/tsl/platform:env",
         "//xla/tsl/platform:errors",
         "//xla/tsl/platform:statusor",

xla/stream_executor/rocm/BUILD
@@ -58,7 +58,6 @@ cc_library(
         "rocm-only",
     ],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_status",
         "//xla/stream_executor/gpu:context",
         "//xla/stream_executor/gpu:scoped_activate_context",
@@ -66,22 +65,6 @@ cc_library(
         "@com_google_absl//absl/log:check",
         "@com_google_absl//absl/status",
         "@local_config_rocm//rocm:rocm_headers",
-    ],
-)
-
-cc_library(
-    name = "rocm_driver_wrapper",
-    hdrs = ["rocm_driver_wrapper.h"],
-    defines = {"__HIP_DISABLE_CPP_FUNCTIONS__": "1"},
-    tags = [
-        "gpu",
-        "rocm-only",
-    ],
-    deps = [
-        "//xla/tsl/platform:env",
-        "@local_config_rocm//rocm:hip",  # buildcleaner: keep
-        "@local_config_rocm//rocm:rocm_headers",
-        "@tsl//tsl/platform:dso_loader",
     ],
 )
 
@@ -94,7 +77,6 @@ cc_library(
         "rocm-only",
     ],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_status",
         "//xla/stream_executor:activate_context",
         "//xla/stream_executor:event",
@@ -107,6 +89,7 @@ cc_library(
         "@com_google_absl//absl/status:statusor",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/strings:str_format",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
     ],
 )
@@ -141,7 +124,6 @@ cc_library(
     deps = [
         ":rocm_command_buffer",
         ":rocm_context",
-        ":rocm_driver_wrapper",
         ":rocm_event",
         ":rocm_kernel",
         ":rocm_platform_id",
@@ -197,6 +179,7 @@ cc_library(
         "@com_google_absl//absl/strings:str_format",
         "@com_google_absl//absl/synchronization",
         "@com_google_absl//absl/types:span",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
         "@tsl//tsl/platform:fingerprint",
         "@tsl//tsl/platform:numbers",
@@ -243,7 +226,6 @@ cc_library(
     ],
     visibility = ["//visibility:public"],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_status",
         "//xla/stream_executor:activate_context",
         "//xla/stream_executor:kernel",
@@ -259,6 +241,7 @@ cc_library(
         "@com_google_absl//absl/status",
         "@com_google_absl//absl/status:statusor",
         "@com_google_absl//absl/strings",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
     ],
 )
@@ -297,7 +280,6 @@ cc_library(
     ],
     visibility = ["//visibility:public"],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_executor",
         ":rocm_platform_id",
         ":rocm_status",
@@ -314,6 +296,7 @@ cc_library(
         "@com_google_absl//absl/status:statusor",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/synchronization",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
     ],
     alwayslink = True,  # Registers itself with the PlatformManager.
@@ -338,24 +321,6 @@ cc_library(
 )
 
 cc_library(
-    name = "rocblas_wrapper",
-    hdrs = ["rocblas_wrapper.h"],
-    tags = [
-        "gpu",
-        "rocm-only",
-    ],
-    deps = [
-        ":rocm_executor",
-        "//xla/tsl/platform:env",
-        "//xla/tsl/util:determinism_for_kernels",
-        "@local_config_rocm//rocm:rocm_headers",
-        "@tsl//tsl/platform",
-        "@tsl//tsl/platform:dso_loader",
-    ],
-    alwayslink = True,
-)
-
-cc_library(
     name = "rocblas_plugin",
     srcs = ["rocm_blas.cc"],
     hdrs = ["rocm_blas.h"],
@@ -367,7 +332,6 @@ cc_library(
     deps = [
         ":hipblas_lt_header",
         ":rocblas_if_static",
-        ":rocblas_wrapper",
         ":rocm_complex_converters",
         ":rocm_executor",
         ":rocm_helpers",
@@ -411,38 +375,6 @@ cc_library(
     deps = if_static([
         "@local_config_rocm//rocm:hipfft",
     ]),
-)
-
-cc_library(
-    name = "rocm_solver_context",
-    srcs = ["rocm_solver_context.cc"],
-    hdrs = ["rocm_solver_context.h"],
-    tags = [
-        "gpu",
-        "manual",
-        "rocm-only",
-    ],
-    deps = [
-        ":hipsolver_wrapper",
-        ":rocblas_wrapper",
-        ":rocm_platform_id",
-        ":rocsolver_wrapper",
-        "//xla:comparison_util",
-        "//xla:util",
-        "//xla:xla_data_proto_cc",
-        "//xla/stream_executor:blas",
-        "//xla/stream_executor:device_address",
-        "//xla/stream_executor:gpu_solver_context",
-        "//xla/stream_executor:stream",
-        "//xla/stream_executor/platform:platform_object_registry",
-        "//xla/tsl/platform:errors",
-        "//xla/tsl/platform:logging",
-        "@com_google_absl//absl/memory",
-        "@com_google_absl//absl/status",
-        "@com_google_absl//absl/status:statusor",
-        "@local_config_rocm//rocm:rocm_headers",
-    ],
-    alwayslink = 1,
 )
 
 cc_library(
@@ -544,25 +476,22 @@ cc_library(
 )
 
 cc_library(
-    name = "hiprand_if_static",
+    name = "hipsolver_wrapper",
+    hdrs = ["hipsolver_wrapper.h"],
     tags = [
         "gpu",
         "rocm-only",
     ],
     deps = if_static([
-        "@local_config_rocm//rocm:hiprand",
-    ]),
-)
-
-cc_library(
-    name = "hipsparse_if_static",
-    tags = [
-        "gpu",
-        "rocm-only",
+        "@local_config_rocm//rocm:hipsolver",
+    ]) + [
+        ":rocm_executor",
+        ":rocm_platform_id",
+        "//xla/tsl/platform:env",
+        "@local_config_rocm//rocm:rocm_headers",
+        "@tsl//tsl/platform:dso_loader",
     ],
-    deps = if_static([
-        "@local_config_rocm//rocm:hipsparse",
-    ]),
+    alwayslink = True,
 )
 
 cc_library(
@@ -573,8 +502,9 @@ cc_library(
         "gpu",
         "rocm-only",
     ],
-    deps = [
-        ":hipsparse_if_static",
+    deps = if_static([
+        "@local_config_rocm//rocm:hipsparse",
+    ]) + [
         ":rocm_executor",
         ":rocm_platform_id",
         "//xla/tsl/platform:env",
@@ -586,14 +516,22 @@ cc_library(
 )
 
 cc_library(
-    name = "rocsolver_if_static",
+    name = "rocblas_wrapper",
+    hdrs = ["rocblas_wrapper.h"],
     tags = [
         "gpu",
         "rocm-only",
     ],
-    deps = if_static([
-        "@local_config_rocm//rocm:rocsolver",
-    ]),
+    deps = [
+        ":rocblas_if_static",
+        ":rocm_executor",
+        "//xla/tsl/platform:env",
+        "//xla/tsl/util:determinism_for_kernels",
+        "@local_config_rocm//rocm:rocm_headers",
+        "@tsl//tsl/platform",
+        "@tsl//tsl/platform:dso_loader",
+    ],
+    alwayslink = True,
 )
 
 cc_library(
@@ -604,37 +542,9 @@ cc_library(
         "gpu",
         "rocm-only",
     ],
-    deps = [
-        ":rocm_executor",
-        ":rocm_platform_id",
-        ":rocsolver_if_static",
-        "//xla/tsl/platform:env",
-        "@local_config_rocm//rocm:rocm_headers",
-        "@tsl//tsl/platform:dso_loader",
-    ],
-    alwayslink = True,
-)
-
-cc_library(
-    name = "hipsolver_if_static",
-    tags = [
-        "gpu",
-        "rocm-only",
-    ],
     deps = if_static([
-        "@local_config_rocm//rocm:hipsolver",
-    ]),
-)
-
-cc_library(
-    name = "hipsolver_wrapper",
-    hdrs = ["hipsolver_wrapper.h"],
-    tags = [
-        "gpu",
-        "rocm-only",
-    ],
-    deps = [
-        ":hipsolver_if_static",
+        "@local_config_rocm//rocm:rocsolver",
+    ]) + [
         ":rocm_executor",
         ":rocm_platform_id",
         "//xla/tsl/platform:env",
@@ -661,7 +571,6 @@ cc_library(
     hdrs = [
         "hip_blas_lt.h",
         "hip_blas_utils.h",
-        "hipblaslt_wrapper.h",
     ],
     defines = {"__HIP_DISABLE_CPP_FUNCTIONS__": "1"},
     tags = [
@@ -713,7 +622,6 @@ cc_library(
     hdrs = [
         "hip_blas_lt.h",
         "hip_blas_utils.h",
-        "hipblaslt_wrapper.h",
     ],
     tags = [
         "gpu",
@@ -756,17 +664,7 @@ cc_library(
         "@com_google_absl//absl/status",
         "@com_google_absl//absl/strings",
     ],
-)
-
-cc_library(
-    name = "roctracer_if_static",
-    tags = [
-        "gpu",
-        "rocm-only",
-    ],
-    deps = if_static([
-        "@local_config_rocm//rocm:roctracer",
-    ]),
+    alwayslink = 1,
 )
 
 cc_library(
@@ -779,7 +677,6 @@ cc_library(
     ],
     deps = [
         ":rocm_executor",
-        ":roctracer_if_static",  # buildcleaner: keep
         "@local_config_rocm//rocm:rocm_headers",
     ],
     alwayslink = True,
@@ -835,7 +732,6 @@ cc_library(
         ":rocblas_plugin",
         ":rocm_helpers",
         ":rocm_platform",
-        ":rocm_solver_context",
         ":topk_kernel_rocm",
     ] + [":cub_sort_kernel_rocm_" + suffix for suffix in get_cub_sort_kernel_types()],
     alwayslink = 1,
@@ -898,7 +794,6 @@ cc_library(
         "rocm-only",
     ],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_event",
         ":rocm_kernel",
         ":rocm_status",
@@ -922,6 +817,7 @@ cc_library(
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/strings:str_format",
         "@com_google_absl//absl/strings:string_view",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
     ],
 )
@@ -961,7 +857,6 @@ cc_library(
         "rocm-only",
     ],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_event",
         ":rocm_status",
         "//xla/stream_executor:activate_context",
@@ -974,6 +869,7 @@ cc_library(
         "@com_google_absl//absl/status",
         "@com_google_absl//absl/status:statusor",
         "@com_google_absl//absl/time",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
     ],
 )
@@ -1049,7 +945,6 @@ cc_library(
         "rocm-only",
     ],
     deps = [
-        ":rocm_driver_wrapper",
         ":rocm_kernel",
         ":rocm_status",
         "//xla/stream_executor:bit_pattern",
@@ -1071,6 +966,7 @@ cc_library(
         "@com_google_absl//absl/status:statusor",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",
+        "@local_config_rocm//rocm:hip",
         "@local_config_rocm//rocm:rocm_headers",
         "@tsl//tsl/platform:casts",
     ],

third_party/gpus/crosstool/BUILD.rocm.tpl
@@ -35,7 +35,7 @@ cc_toolchain_suite(
 
 cc_toolchain(
     name = "cc-compiler-local",
-    all_files = "@local_config_rocm//rocm:all_files",
+    all_files = ":crosstool_wrapper_driver_is_not_gcc",
     compiler_files = ":crosstool_wrapper_driver_is_not_gcc",
     ar_files = ":crosstool_wrapper_driver_is_not_gcc",
     as_files = ":crosstool_wrapper_driver_is_not_gcc",

third_party/gpus/rocm/build_defs.bzl.tpl
@@ -1,3 +1,4 @@
+load("@rules_cc//cc:cc_import.bzl", "cc_import")
 load("@rules_cc//cc:cc_library.bzl", "cc_library")
 
 # Macros for building ROCm code.
@@ -10,17 +11,8 @@ def if_rocm(if_true, if_false = []):
     """
     return select({
         "@local_config_rocm//rocm:using_hipcc": if_true,
-        "//conditions:default": if_false
+        "//conditions:default": if_false,
     })
-
-def select_threshold(value, threshold_dict):
-    sorted_keys = sorted(threshold_dict.keys())
-    result = threshold_dict[sorted_keys[0]]  # Default to the first threshold's value
-    for key in sorted_keys:
-        if value >= key:
-            result = threshold_dict[key]
-
-    return result
 
 def rocm_default_copts():
     """Default options for all ROCm compilations."""
@@ -81,14 +73,38 @@ def rocm_hipblaslt():
 
 def if_rocm_hipblaslt(x):
     if %{rocm_is_configured} and (%{rocm_hipblaslt} == "True"):
-      return select({"//conditions:default": x})
+        return select({"//conditions:default": x})
     return select({"//conditions:default": []})
 
 def rocm_library(copts = [], deps = [], **kwargs):
     """Wrapper over cc_library which adds default ROCm options."""
     if "@local_config_rocm//rocm:rocm_headers" not in deps:
-      deps.append("@local_config_rocm//rocm:rocm_headers")
+        deps.append("@local_config_rocm//rocm:rocm_headers")
     cc_library(copts = rocm_default_copts() + copts, deps = deps, **kwargs)
 
 def get_rbe_amdgpu_pool(is_single_gpu = False):
     return "%{single_gpu_rbe_pool}" if is_single_gpu else "%{multi_gpu_rbe_pool}"
+
+def rocm_lib_import(name, interface_library, data, deps):
+    cc_import(
+        name = name + "_interface",
+        interface_library = interface_library,
+        system_provided = True,
+        visibility = ["//visibility:private"],
+    )
+    cc_library(
+        name = name + "_libs",
+        data = data,
+        deps = deps,
+        visibility = ["//visibility:private"],
+    )
+    cc_library(
+        name = name,
+        deps = [
+            ":{}_interface".format(name),
+            ":{}_libs".format(name),
+            ":rocm_headers_includes",
+            ":rocm_rpath",
+        ],
+        visibility = ["//visibility:public"],
+    )

third_party/gpus/rocm/rocm_config.h.tpl
@@ -22,15 +22,5 @@ limitations under the License.
 #define TF_MIOPEN_VERSION %{miopen_version_number}
 #define TF_HIPRUNTIME_VERSION %{hipruntime_version_number}
 #define TF_HIPBLASLT %{hipblaslt_flag}
-#define TF_HIPRUNTIME_SOVERSION "%{hip_soversion_number}"
-#define TF_ROCBLAS_SOVERSION "%{rocblas_soversion_number}"
-#define TF_HIPBLASLT_SOVERSION "%{hipblaslt_soversion_number}"
-#define TF_MIOPEN_SOVERSION "%{miopen_soversion_number}"
-#define TF_HIPFFT_SOVERSION "%{hipfft_soversion_number}"
-#define TF_ROCSOLVER_SOVERSION "%{rocsolver_soversion_number}"
-#define TF_HIPSPARSE_SOVERSION "%{hipsparse_soversion_number}"
-#define TF_ROCTRACER_SOVERSION "%{roctracer_soversion_number}"
-#define TF_HIPSOLVER_SOVERSION "%{hipsolver_soversion_number}"
-#define TF_ROCRAND_SOVERSION "%{rocrand_soversion_number}"
 
 #endif  // ROCM_ROCM_CONFIG_H_

commit f7d005c57fcaac805e79f911153ab748e73f0fa6

Cherry pick/profiler v1 build fix v092 (#873)
* Fix v1 profiler build by guarding rocprofiler-sdk calls (#871)

* remove rocprofiler-sdk deps for v1 build

* added backend comment

(cherry picked from commit 28d1cbc3e4b3305202ab297674682839f35df9a9)

* [ROCm] Fix BUILD syntax error in xla/backends/profiler/gpu

Add missing comma after the `deps = select({...})` block in the
`rocm_profiler_backend_cfg` rule. Without this, Bazel fails to parse
the BUILD file with:

  syntax error at 'visibility': expected ,

Typo was introduced by #805 ("Streamline bazel targets for rocm
libraries (#805)"); the equivalent #804 on rocm-jaxlib-v0.9.1 has the
comma. No functional change.

* [ROCm] clang-format: sort rocm/* includes with other third_party includes

Move the rocm/include/hip, rocm/include/rocprofiler-sdk, rocm/include/roctracer
(in rocm_collector.cc) and rocm/include/hip, rocprofiler-sdk (in rocm_collector.h)
includes into alphabetical position between the absl/ and xla/ groups, to
satisfy the v0.9.2 lint/format check on PR #873.

The upstream commit (28d1cbc / #871) placed these at the bottom of the include
list; v0.9.2 enforces stricter include ordering and requires them sorted with
the other third_party headers. Pure include reorder, no functional change.

v1 backend build verified locally:
  bazel build --config=rocm --define=xla_rocm_profiler=v1 \
    //xla/backends/profiler/gpu:rocm_collector  -> OK

* [ROCm] Add @tsl//tsl/platform:dso_loader dep to rocm_tracer_impl

RBE (rocm_rbe config) enforces strict header checking and fails the
compile of rocm_profiler_sdk.cc with:

  undeclared inclusion(s) in rule '//xla/backends/profiler/gpu:rocm_tracer_impl':
    'external/tsl/tsl/platform/dso_loader.h'
    'xla/tsl/platform/default/dso_loader.h'

The header is pulled in transitively (via tsl/platform/env.h or platform_port);
declare the dep explicitly to satisfy strict-headers and unblock CI on v0.9.2.

No code change.

xla/backends/profiler/gpu/BUILD
@@ -455,7 +455,7 @@ cc_library(
         ":use_v1": ["@local_config_rocm//rocm:roctracer"],
         ":use_rocprofiler_sdk": ["@local_config_rocm//rocm:rocprofiler_sdk"],
         "//conditions:default": ["@local_config_rocm//rocm:rocprofiler_sdk"],
-    })
+    }),
     visibility = ["//visibility:public"],
 )
 
@@ -579,6 +579,7 @@ cc_library(
         "@com_google_absl//absl/status",
         "@com_google_absl//absl/synchronization",
         "@local_config_rocm//rocm:rocm_headers",
+        "@tsl//tsl/platform:dso_loader",
         "@tsl//tsl/platform:env",
         "@tsl//tsl/platform:errors",
         "@tsl//tsl/platform:logging",

xla/backends/profiler/gpu/rocm_collector.cc
@@ -136,6 +136,19 @@ void PrintRocmTracerEvent(const RocmTracerEvent& event,
   VLOG(3) << oss.str() << ' ' << message;
 }
 
+#if XLA_GPU_ROCM_TRACER_BACKEND == XLA_GPU_ROCM_TRACER_BACKEND_V1
+static uint64_t get_timestamp() {
+  uint64_t ts;
+  if (roctracer_get_timestamp(&ts) != ROCTRACER_STATUS_SUCCESS) {
+    const char* errstr = roctracer_error_string();
+    LOG(ERROR) << "function roctracer_get_timestamp failed with error "
+               << errstr;
+    // Return 0 on error.
+    return 0;
+  }
+  return ts;
+}
+#else
 uint64_t get_timestamp() {
   uint64_t ts;
   rocprofiler_status_t CHECKSTATUS = rocprofiler_get_timestamp(&ts);
@@ -147,6 +160,7 @@ uint64_t get_timestamp() {
   }
   return ts;
 }
+#endif  // XLA_GPU_ROCM_TRACER_BACKEND == XLA_GPU_ROCM_TRACER_BACKEND_V1
 }  // namespace
 
 OccupancyStats PerDeviceCollector::GetOccupancy(

xla/backends/profiler/gpu/rocm_collector.h
@@ -36,6 +36,19 @@ limitations under the License.
 #include "xla/tsl/profiler/utils/xplane_builder.h"
 #include "tsl/profiler/protobuf/xplane.pb.h"
 
+// Backend: 3=v3 (rocprofiler-sdk), 1=v1 (roctracer). Default to v3.
+#if !defined(XLA_GPU_ROCM_TRACER_BACKEND_V3)
+#define XLA_GPU_ROCM_TRACER_BACKEND_V3 3
+#endif
+
+#if !defined(XLA_GPU_ROCM_TRACER_BACKEND_V1)
+#define XLA_GPU_ROCM_TRACER_BACKEND_V1 1
+#endif
+
+#ifndef XLA_GPU_ROCM_TRACER_BACKEND
+#error "XLA_GPU_ROCM_TRACER_BACKEND not defined"
+#endif
+
 namespace xla {
 namespace profiler {
 