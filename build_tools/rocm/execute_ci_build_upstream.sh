bc512f999917cec1eb8cd478a175d43a5dd0bd64
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-06-02 19:16:06 +0200

    Update execute_ci_build_upstream.sh

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -68,6 +68,7 @@ done
     --nokeep_going \
     --repo_env=TF_ROCM_RBE_SINGLE_GPU_POOL=${RBE_POOL} \
     --repo_env=TF_ROCM_RBE_SINGLE_GPU_POOL=linux_x64_gpu_do_gfx950 \
+    --remote_cache_compression \
     -- \
     //xla/... \
     -//xla/tests:dot_operation_test_amdgpu_any \
