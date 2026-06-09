0756f1c25bdc668626805ac20f959cfee9f0e1c5
Milica Makevic <Milica.Makevic@amd.com>
2026-06-09 18:16:54 +0000

    Add CUBLASLT_FISSION to the list of disabled autotune backends

--- xla/service/gpu/autotuning/autotuner_pass.cc
+++ xla/service/gpu/autotuning/autotuner_pass.cc
@@ -331,6 +331,7 @@ AutotunerPass::GetGpuAutotunerBackends(
     disabled_autotune_backends.push_back(autotuner::Backend::HIPBLASLT);
     disabled_autotune_backends.push_back(autotuner::Backend::MIOPEN);
     disabled_autotune_backends.push_back(autotuner::Backend::HIPBLASLT_FISSION);
+    disabled_autotune_backends.push_back(autotuner::Backend::CUBLASLT_FISSION);
   }
 
   if (!debug_options.xla_gpu_experimental_autotune_post_fusion() ||
