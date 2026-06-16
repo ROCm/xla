a15594769087a20a70e5dfdc00a8295758a6467e
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-06-16 16:54:50 +0200

    Clean-up fixed tests (#966)
    * Clean-up fixed tests
    
    Removed several excluded tests from the CI build script.
    
    * Add TritonEmitterTest to excluded tests list
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh
    
    * Update execute_ci_build_upstream.sh

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -11,8 +11,6 @@ EXCLUDED_TESTS=(
     "TritonAndBlasSupportForDifferentTensorSizes/TritonAndBlasSupportForDifferentTensorSizes.IsDotAlgorithmSupportedByTriton/dot_bf16_bf16_f32_x*"
     "F8E5M2Tests/DotAlgorithmSupportTest.AlgorithmIsSupportedFromCudaCapability/dot_any_f8_any_f8_f32_*"
     "DotOperationTestWithCublasLt_F16F32F64CF64/1.GeneralMatMulActivation"
-    "DotOperationTestForBatchMatMul/1*"
-    "ParameterizedGemmRewriteTest.GemmTypeCombinationCheck"
     "MatmulTestWithCublas.GemmRewriter_RegressionTestF64"
     "TritonEmitterTest.ScaledDotIsSupportedByReferencePlatform"
     "SampleFileTest.Convolution" # https://wardite.cluster.engflow.com/invocations/default/c105260c-873a-4634-ab69-905a43ca5cf8
