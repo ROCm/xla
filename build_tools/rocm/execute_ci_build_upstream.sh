53e45ed071f65611e992954fab7a4be35d216ddf
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-07-31 10:14:30 +0200

    Update execute_ci_build_upstream.sh

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -20,6 +20,7 @@ EXCLUDED_TESTS=(
     "VmmTest*"
     "MxScaledDotExecutionTest.MxFp4Fp8MixedBatchedCorrectness"
     "ConvolutionTest.Convolve3D_1x4x2x3x3_2x2x2x3x3_Valid"
+    "RocmMemoryReservationTest.RemapRepointsRequiredSlice"
 )
 
 TAG_FILTERS=$("${SCRIPT_DIR}/rocm_tag_filters.sh")
