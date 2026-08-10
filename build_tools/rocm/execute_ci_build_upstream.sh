3b679bd490206180b13d73a7b4c2cd617c64a8d3
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2026-08-10 15:46:10 -0500

    Quick fix - disable RecordFfiTest.KernelLaunchBoundFfi
    Untill https://github.com/openxla/xla/pull/46972 gets merged

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -9,6 +9,7 @@ EXCLUDED_TESTS=(
     "NumericTestsForBlas/NumericTestsForBlas.Infinity/dot_tf32_tf32_f32_x3"
     "TritonEmitterTest.ScaledDotIsSupportedByReferencePlatform"
     "VmmTest.CommandBufferSkipProfiledTwoGemmChain"
+    "RecordFfiTest.KernelLaunchBoundFfi"
 )
 
 TAG_FILTERS=$("${SCRIPT_DIR}/rocm_tag_filters.sh")
