dbee78fb0d315c1a95e3dad71e540b817b1ff851
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2026-08-12 05:06:01 -0500

    [ROCm] Skip VmmTest.CommandBufferSkipProfiledTwoGemmChain again

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -8,6 +8,7 @@ EXCLUDED_TESTS=(
     "HostMemoryAllocateTest.Numa"                                                                                                                  # Failing on RBE
     "NumericTestsForBlas/NumericTestsForBlas.Infinity/dot_tf32_tf32_f32_x3"
     "TritonEmitterTest.ScaledDotIsSupportedByReferencePlatform"
+    "VmmTest.CommandBufferSkipProfiledTwoGemmChain"
     "GpuKernelTilingTest.ReductionInputTooLarge"
     "MxScaledDotExecutionTest.MxFp4Fp8MixedBatchedCorrectness"
     "ConvolutionTest.Convolve3D_1x4x2x3x3_2x2x2x3x3_Valid"
