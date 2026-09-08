c03c76a3df3f3e257fc1f2ecd2ba3f5e42b6b40c
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-09-08 14:44:08 +0200

    Enable test sharding for upstream xla (#1167)
    * Enable test sharding for upstream xla
    
    * Add pkgs read permission
    
    * Fix
    
    * Inherit secrets
    
    * Test hight load
    
    * Enable sharding
    
    * Increase number of jobs to 60
    
    * Reduce number of jobs to 50
    
    * Trigger CI/CD pipeline
    
    * Clean-up

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -43,13 +43,13 @@ done
     --repo_env=TF_ROCM_AMDGPU_TARGETS=gfx950 \
     --remote_download_outputs=minimal \
     --grpc_keepalive_time=30s \
-    --test_sharding_strategy=disabled \
+    --test_sharding_strategy=explicit \
     --test_verbose_timeout_warnings \
     --test_timeout=920,2400,7200,9600 \
     --sandbox_add_mount_pair=/dev/null:/etc/ld.so.cache \
     --curses=no \
     --color=yes \
-    --jobs=30 \
+    --jobs=50 \
     --test_filter=-$(
         IFS=:
         echo "${EXCLUDED_TESTS[*]}"
