56f1cdaeda0cf6bf906da3eabaf61bf059540ba8
Alexandros Theodoridis <atheodor@amd.com>
2026-09-08 08:14:45 +0000

    Test hight load

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -46,6 +46,8 @@ done
     --test_verbose_timeout_warnings \
     --test_timeout=920,2400,7200,9600 \
     --sandbox_add_mount_pair=/dev/null:/etc/ld.so.cache \
+    --cache_test_results=no \
+    --test_env=TEST="$(date)" \
     --curses=no \
     --color=yes \
     --jobs=30 \
@@ -54,7 +56,7 @@ done
         echo "${EXCLUDED_TESTS[*]}"
     ) \
     --cache_test_results=yes \
-    --nokeep_going \
+    --keep_going \
     --repo_env=TF_ROCM_RBE_SINGLE_GPU_POOL=linux_x64_gpu_do_gfx950 \
     -- \
     //xla/...
