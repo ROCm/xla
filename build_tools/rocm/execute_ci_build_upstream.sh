e479378ad8d26e9a7daf97c2b6de23d73a69aa51
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-09-15 09:59:33 +0200

    Remove remote_download_outputs from CI build script
    Removed remote download outputs option for CI build.

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -40,7 +40,6 @@ done
     --execution_log_compact_file=execution_log.binpb.zst \
     --repo_env=REMOTE_GPU_TESTING=1 \
     --repo_env=TF_ROCM_AMDGPU_TARGETS=gfx950 \
-    --remote_download_outputs=minimal \
     --grpc_keepalive_time=30s \
     --test_sharding_strategy=explicit \
     --test_verbose_timeout_warnings \
