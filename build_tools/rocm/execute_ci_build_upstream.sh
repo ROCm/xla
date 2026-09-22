1b00a9e8515d4aeb383bf1aa2c5fd24ef2693edf
Alexandros Theodoridis <alexandros.theodoridis@amd.com>
2026-09-22 12:33:58 +0200

    Exclude StreamExecutorGpuClientTest.NumaNode to filters
    Numa issue

--- build_tools/rocm/execute_ci_build_upstream.sh
+++ build_tools/rocm/execute_ci_build_upstream.sh
@@ -9,6 +9,7 @@ EXCLUDED_TESTS=(
     "ConvolutionTest.Convolve_1x1x4x4_1x1x2x2_Valid*"
     "ConvolutionTest.Convolve_1x1x4x4_1x1x2x2_Same*"
     "ConvolutionTest.Convolve_1x1x4x4_1x1x3x3_Same*"
+    "StreamExecutorGpuClientTest.NumaNode"
 )
 
 TAG_FILTERS=$("${SCRIPT_DIR}/rocm_tag_filters.sh")
